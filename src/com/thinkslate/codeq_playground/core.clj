9b79701e5122c09ad4c573466707d2f629e117a5
Devin Walters <devinw@gmail.com>
2012-11-25 14:28:51 -0600

    Use clojure 1.5.0-alpha6, add example queries from codeq unsession

--- src/com/thinkslate/codeq_playground/core.clj
+++ src/com/thinkslate/codeq_playground/core.clj
@@ -1,11 +1,17 @@
 (ns com.thinkslate.codeq-playground.core
-  (:require [datomic.api :as d]))
+  (:require [datomic.api :as d]
+            [clojure.pprint :refer [pprint]]))
 
-(def uri "datomic:free://localhost:4334/clojure")
+ ;; Change me to the db-name you used when running ./script/analyze-repo
+(def db-name "clojure")
+
+(def uri (str "datomic:free://localhost:4334/" db-name))
 (def conn (d/connect uri))
 (def db (d/db conn))
 
-(def rules
+(def
+  ^{:doc "http://blog.datomic.com/2012/10/codeq.html"}
+  rules
   '[[(node-files ?n ?f) [?n :node/object ?f] [?f :git/type :blob]]
     [(node-files ?n ?f) [?n :node/object ?t] [?t :git/type :tree]
      [?t :tree/nodes ?n2] (node-files ?n2 ?f)]
@@ -29,9 +35,107 @@
          [?cq :codeq/file ?f]
          (file-commits ?f ?c)
          (?c :commit/authoredAt ?date)]
-       db rules "clojure.core/pmap"))
+       db rules fq-fn))
+;; (find-commits-for-fn "clojure.core/map")
+
+(defn get-committers []
+  (d/q '[:find ?email
+         :where
+         [_ :commit/committer ?u]
+         [?u :email/address ?email]]
+       db))
+;; (get-committers)
+
+(defn get-authors []
+  (d/q '[:find ?email
+         :where
+         [_ :commit/author ?e]
+         [?u :email/address ?email]]
+       db))
+;; (get-authors)
+
+(defn initial-commit-date []
+  (d/q '[:find (min ?date)
+         :where
+         [_ :commit/committedAt ?date]]
+       db))
+;; (initial-commit-date)
+
+(defn num-commits []
+  (d/q '[:find (count ?c)
+         :where
+         [?c :git/type :commit]]
+       db))
+;; (num-commits)
+
+(defn top-n-committers [n]
+  (->> (d/q '[:find ?email (count ?commit)
+              :where
+              [?commit :commit/author ?author]
+              [?author :email/address ?email]]
+            db)
+       (sort-by second)
+       reverse
+       (take n)))
+;; (top-n-committers 5)
+
+(defn first-and-latest-commit-by-author-email [email]
+  (d/q '[:find ?email (min ?date) (max ?date)
+         :in $ ?email
+         :where
+         [?e :commit/committedAt ?date]
+         [?e :commit/author ?u]
+         [?u :email/address ?email]]
+       db email))
+;; (first-and-latest-commit-by-author-email "chouser@n01se.net")
+
+;; Find insts when a codeq was changed
+(defn commit-dates [name]
+  (map first
+       (d/q '[:find (min ?date) ?sha
+              :in $ % ?name
+              :where
+              [?n :code/name ?name]
+              [?cq :clj/def ?n]
+              [?cq :codeq/code ?c]
+              [?c :code/sha ?sha]
+              (codeq-commits ?cq ?commit)
+              [?commit :commit/committedAt ?date]]
+            db rules name)))
+;; (commit-dates "clojure.core/pmap")
+
+;; Match insts to committer names
+(defn committer-by-insts [insts]
+  (d/q '[:find ?email
+         :in $ [?inst ...]
+         :where
+         [?commit :commit/committedAt ?inst]
+         [?commit :commit/committer ?u]
+         [?u :email/address ?email]]
+       db
+       insts))
+;; (-> "clojure.core/pmap" commit-dates committer-by-insts)
+
+
+;; The following blows the heap:
+;; (->> (d/q '[:find ?name (count ?date)
+;;             :in [[?name ?date ?sha]]]
+;;           (d/q '[:find ?name (min ?date) ?sha
+;;                  :in $ %
+;;                  :where
+;;                  [?n :code/name ?name]
+;;                  [?cq :clj/def ?n]
+;;                  [?cq :codeq/code ?c]
+;;                  [?c :code/sha ?sha]
+;;                  (codeq-commits ?cq ?commit)
+;;                  [?commit :commit/committedAt ?date]]
+;;                db
+;;                rules))
+;;      (sort-by second)
+;;      reverse
+;;      (take 5))
 
 (comment
-  ;; Delete a DB
-  (d/delete-database "datomic:free://localhost:4334/YOUR_DB_NAME")
+  ;; Delete your DB
+  (d/delete-database (str "datomic:free://localhost:4334/" db-name))
 )
